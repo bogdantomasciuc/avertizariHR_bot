7e15dafb76dd1a2f8c6f6573f8eef66ad1c62fc8
Bogdan Tomasciuc <bogdan.tomasciuc@atacpc.ro>
2022-10-04 16:36:48 +0300

    First commit.

---
+++ index.js
@@ -0,0 +1,7 @@
+/**************************************************
+ * Bot notificare prezenta Mattermost prin consultarea inregistrarilor 
+ * din tabela de prezenta create prin aplicatia HR SelfService Online
+ * 
+ * Cheia de legatura este user_id care se salveaza in tabela personal din Nexus in campul id_extern
+ * 
+ * */ 
